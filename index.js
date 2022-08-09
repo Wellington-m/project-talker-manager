a2f20046ed9c68bc087281a39a9aaa81e3325c78
Wellington <wellingtonmachioni@gmail.com>
2022-08-09 13:50:29 -0300

    Adicionado rota para trazer um talker pelo id

--- index.js
+++ index.js
@@ -22,6 +22,21 @@ app.get('/talker', async (_req, res) => {
   res.status(200).json(talkers);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const fileContent = await fs.readFile('./talker.json', 'utf-8');
+  const talkers = JSON.parse(fileContent);
+
+  const { id } = req.params;
+
+  const selectedTalker = talkers.find((talker) => talker.id === Number(id));
+
+  if (!selectedTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(200).json(selectedTalker);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
