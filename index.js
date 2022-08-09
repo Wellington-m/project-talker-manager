007d2cb7d6e37fbaf11d3a44fa9ac05e19890d2d
Wellington <wellingtonmachioni@gmail.com>
2022-08-09 15:23:24 -0300

    Implementado validações para o email e a senha

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const generateRandomToken = (num) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let token = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < num; i += 1) {
+    token += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return token;
+};
+
+const validateEmail = (email) => {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -39,19 +55,19 @@ app.get('/talker/:id', async (req, res) => {
 
 app.post('/login', (req, res) => {
   const TOKEN_LENGTH = 16;
+  const EMAIL_NULL_MESSAGE = 'O campo "email" é obrigatório';
+  const INVALID_EMAIL_MESSAGE = 'O "email" deve ter o formato "email@email.com"';
+  const PASSWORD_NULL_MESSAGE = 'O campo "password" é obrigatório';
+  const PASSWORD_LENGTH_MESSAGE = 'O "password" deve ter pelo menos 6 caracteres';
 
-  const generateRandomToken = (num) => {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let token = '';
-    const charactersLength = characters.length;
-    for (let i = 0; i < num; i += 1) {
-      token += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-
-    return token;
-  };
+  const { email, password } = req.body;
 
   const randomToken = generateRandomToken(TOKEN_LENGTH);
+
+  if (!email) return res.status(400).json({ message: EMAIL_NULL_MESSAGE });
+  if (!validateEmail(email)) return res.status(400).json({ message: INVALID_EMAIL_MESSAGE });
+  if (!password) return res.status(400).json({ message: PASSWORD_NULL_MESSAGE });
+  if (password.length < 6) return res.status(400).json({ message: PASSWORD_LENGTH_MESSAGE });
   
   res.status(200).json({ token: `${randomToken}` });
 });
